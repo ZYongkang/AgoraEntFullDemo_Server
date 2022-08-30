bf44ff5ec39c39bb608127de98d0b7568d745364
newBlashOfKing <18401432360@163.com>
2022-08-30 10:13:00 +0800

    add get update easemob create user im api

--- src/main/java/com/md/mic/model/EasemobUser.java
+++ src/main/java/com/md/mic/model/EasemobUser.java
@@ -3,10 +3,12 @@ package com.md.mic.model;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @TableName("easemob_user")
+@Builder
 public class EasemobUser {
 
     @TableId(value = "id", type = IdType.AUTO)
