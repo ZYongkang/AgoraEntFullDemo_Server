79931237b83e99d03149cedc2a4de51cfc3318d0
ZYongkang <zhangyk_01@163.com>
2022-09-19 10:34:21 +0800

    fixed run failed

--- src/main/java/com/md/mic/service/impl/MicApplyUserServiceImpl.java
+++ src/main/java/com/md/mic/service/impl/MicApplyUserServiceImpl.java
@@ -188,7 +188,7 @@ public class MicApplyUserServiceImpl extends ServiceImpl<MicApplyUserMapper, Mic
             UserDTO userDTO = ownerMap.get(applyUser.getUid());
             long createdAt = applyUser.getCreatedAt().toInstant(ZoneOffset.of(zoneOffset))
                     .toEpochMilli();
-            list.add(MicApplyVO.builder().user(userDTO).index(applyUser.getMicIndex())
+            list.add(MicApplyVO.builder().member(userDTO).index(applyUser.getMicIndex())
                     .createdAt(createdAt).build());
         }
         PageInfo<MicApplyVO> pageInfo = new PageInfo<>();
